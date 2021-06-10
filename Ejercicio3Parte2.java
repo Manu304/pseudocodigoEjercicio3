8383bb126b05931ddc514951d47aaac5c3a2b78d
Manuel Rojas <manuerjs70@gmail.com>
2021-06-09 19:25:42 -0600

    Agregue gitignore para archivos compilados y el codigo java del ejercicio3

---
+++ Ejercicio3Parte2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+//202030799 Manuel Rojas
+//Se pide desarrollar un programa que lee un valor N y determine su factorial.
+public class Ejercicio3Parte2 {
+    public static void main(String[] args) {
+        int num = 0;
+        int factorial = 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero entero para calcular su factorial: ");
+        if (num > 0){
+            while(num > 1){
+                factorial *= num;
+                num--;
+            }
+        }
+        System.out.println("El factorial de " + num + " es: " + factorial);
+    }
+}
