588b07d7ca8a6ba36797c3aaf4497a49d8cad984
Manuel Rojas <manuerjs70@gmail.com>
2021-06-09 19:31:43 -0600

    Agregue variables a ejercicio3

--- Ejercicio3Parte2.java
+++ Ejercicio3Parte2.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 //Se pide desarrollar un programa que lee un valor N y determine su factorial.
 public class Ejercicio3Parte2 {
     public static void main(String[] args) {
-        int num = 0, producto = 0;
+        int num = 0, original = 0;
         int factorial = 1;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese un numero entero para calcular su factorial: ");
+        System.out.print("Ingrese un numero entero para calcular su factorial: ");
         num = scanner.nextInt();
+        original = num;
         if (num > 0){
-            producto = num;
             while(num > 1){
-                factorial *= producto;
-                producto--;
+                factorial *= num;
+                num--;
             }
         }
-        System.out.println("El factorial de " + num + " es: " + factorial);
+        System.out.println("El factorial de " + original + " es: " + factorial);
     }
 }
